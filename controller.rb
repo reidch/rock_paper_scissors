bfb1e059e1b52502f5b5b83caf67b44fc73505c1
reidch <carolinehreid@gmail.com>
2017-10-09 21:55:03 +0100

    Add files via upload

--- controller.rb
+++ controller.rb
@@ -5,9 +5,13 @@ require_relative 'models/game'
 
 game = Game.new()
 
-get '/:guess1/:guess2' do
-	guess1 = params[:guess1]
-	guess2 = params[:guess2]
-	@victor = game.result(guess1, guess2)
+get '/welcome' do
+	erb( :welcome )
+end
+
+get '/:hand1/:hand2' do
+	hand1 = params[:hand1]
+	hand2 = params[:hand2]
+	@winner = game.result(hand1, hand2)
 	erb( :result )
 end
--- models/game.rb
+++ models/game.rb
@@ -1,16 +1,16 @@
 class Game
 
-	def result(guess1, guess2)
-		if guess1 == guess2
-			return "It's a draw!"
-		elsif (guess1 == "rock" && guess2 == "scissors") || (guess1 == "scissors" && guess2 == "rock")
+	def result(hand1, hand2)
+		if hand1 == hand2
+			return "It's a draw! Have another go!"
+		elsif (hand1 == "rock" && hand2 == "scissors") || (hand1 == "scissors" && hand2 == "rock")
 			return "Rock triumphs!"
-		elsif (guess1 == "paper" && guess2 == "rock") || (guess1 == "rock" && guess2 == "paper")
+		elsif (hand1 == "paper" && hand2 == "rock") || (hand1 == "rock" && hand2 == "paper")
 			return "Paper triumphs!"
-		elsif (guess1 == "scissors" && guess2 == "paper") || (guess1 == "paper" && guess2 == "scissors")
+		elsif (hand1 == "scissors" && hand2 == "paper") || (hand1 == "paper" && hand2 == "scissors")
 			return "Scissors triumphs!"
-		elsif (guess1 || guess2) != "rock" || "paper" || "scissors"
-			return "One of you is a fool! Make sure your next guess is either rock, paper or scissors!"
+		elsif (hand1 || hand2) != "rock" || "paper" || "scissors"
+			return "No winner this time. Make sure your next guess is either rock, paper or scissors!"
 		end
 	end
 end
